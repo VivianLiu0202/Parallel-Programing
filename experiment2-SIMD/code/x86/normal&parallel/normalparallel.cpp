9aa1202637a1d2c1e2ba55ec849a8f6e545b2afe
LiuVivian <3210297430@qq.com>
2023-03-31 15:55:30 +0800

    normal算法修改

---
+++ experiment2-SIMD/code/x86/normal&parallel/normalparallel.cpp
@@ -0,0 +1,144 @@
+#include<iostream>
+#include<windows.h>
+#include<xmmintrin.h>
+#include<emmintrin.h>
+
+using namespace std;
+
+float a[2000][2000],b[2000][2000],c[2000][2000],d[2000][2000];
+void Initialize(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        //对角线元素初始化为1
+        a[i][i]=1.0;
+        b[i][i]=1.0;
+        c[i][i]=1.0;
+        d[i][i]=1.0;
+
+        //下三角元素初始化为0
+        for(int j=0;j<i;j++)
+        {
+            a[i][j] = 0;
+            b[i][j] = 0;
+            c[i][j] = 0;
+            d[i][j] = 0;
+        }
+
+        //上三角元素初始化为随机数
+        for(int j = i+1;j<n;j++)
+        {
+            a[i][j] = rand();
+            b[i][j] = a[i][j];
+            c[i][j] = a[i][j];
+            d[i][j] = a[i][j];
+        }
+    }
+    for(int k = 0;k<n;k++)
+    {
+        for(int i= k+1;i<n;i++)
+        {
+            for(int j = 0;j<n;j++)
+            {
+                //最终每一行的值是上一行的值与这一行的值之和
+                a[i][j]+=a[k][j];
+                b[i][j]+=b[k][j];
+                c[i][j]+=c[k][j];
+                d[i][j]+=d[k][j];
+            }
+        }
+    }
+}
+
+//串行算法
+void Gauss_Normal(int n)
+{
+    for(int k = 0;k<n;k++)
+    {
+        float temp = a[k][k];
+        for(int j = k;j<n;j++)
+            a[k][j]/=temp;
+        for(int i  = k+1;i<n;i++)
+        {
+            //float temp2 = a[i][k];
+            for(int j = k+1;j<n;j++)
+                a[i][j] -= a[i][k]*a[k][j];
+            a[i][k] =0;
+        }
+    }
+}
+
+void Print(int n,float m[][2000])
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j = 0;j<n;j++)
+        {
+            cout<<m[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int N = 1024;
+    int count;
+    int cycle;
+    LARGE_INTEGER t1,t2,tc1,t3,t4,tc2;
+    for(int n = 2;n<=N;n *= 2)
+    {
+        Initialize(n);
+        count = 1;
+        if(n<=30)
+            cycle=1000;
+        else
+            if(n<=70)
+                cycle=100;
+            else
+                if(n>=300)
+                    cycle=10;
+        QueryPerformanceFrequency(&tc1);
+		QueryPerformanceCounter(&t1);
+		while(count<cycle){
+			Gauss_Normal(n);
+            count++;
+        }
+        QueryPerformanceCounter(&t2);
+        cout<<n<<" "<<count<<" "<<((t2.QuadPart - t1.QuadPart)*1000.0 / tc1.QuadPart)<<endl;
+        count=1;
+        QueryPerformanceFrequency(&tc2);
+    }
+    return 0;
+}
+// }
+// for(n=2;n<=N;n*=2){
+// 		Initialize(n);
+// 		count=1;
+// 		if(n<=30)
+//             cycle=1000;
+//         else
+//             if(n<=70)
+//                 cycle=100;
+//             else
+//                 if(n>=300)
+//                     cycle=10;
+
+// 		QueryPerformanceFrequency(&tc1);
+// 		QueryPerformanceCounter(&t1);
+// 		while(count<cycle){
+// 			Gauss_normal(n);
+//             count++;}
+//         QueryPerformanceCounter(&t2);
+// 		cout<<n<<" "<<count<<" "<<((t2.QuadPart - t1.QuadPart)*1000.0 / tc1.QuadPart);
+
+//         count=1;
+// 		QueryPerformanceFrequency(&tc2);
+// 		// QueryPerformanceCounter(&t3);
+// 		// while(count<cycle){
+// 		// 	Gauss_para(n);
+//         //     count++;}
+//         // QueryPerformanceCounter(&t4);
+// 		// cout<<" "<<((t4.QuadPart - t3.QuadPart)*1000.0 / tc2.QuadPart)<<endl;
+// 	}
+// }
