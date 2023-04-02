84f3fed856e3bd789135741127f52d98bd6f3a98
LiuVivian <3210297430@qq.com>
2023-04-02 13:21:59 +0800

    SSE

--- experiment2-SIMD/code/x86/normal&parallel/normalparallel.cpp
+++ experiment2-SIMD/code/x86/normal&parallel/normalparallel.cpp
@@ -57,18 +57,108 @@ void Gauss_Normal(int n)
     {
         float temp = a[k][k];
         for(int j = k;j<n;j++)
-            a[k][j]/=temp;
+            a[k][j]/=temp;  //可以进行向量化，用SIMD并行优化
         for(int i  = k+1;i<n;i++)
         {
             //float temp2 = a[i][k];
             for(int j = k+1;j<n;j++)
-                a[i][j] -= a[i][k]*a[k][j];
+                a[i][j] -= a[i][k]*a[k][j];//可以进行向量化
             a[i][k] =0;
         }
     }
 }
 
-void Print(int n,float m[][2000])
+void Gauss_Part1(int n)//对第一个部分进行向量化的SSE并行算法,采用4路向量化
+{
+    int i,j,k;
+    __m128 t1,t2,t3; //定义4个向量寄存器
+    for(k=0;k<n;k++)
+    {
+        float temp[4] = {b[k][k],b[k][k],b[k][k],b[k][k]};
+        t1 = _mm_loadu_ps(temp);//加载到t1向量寄存器
+        for(j = k+1;j+4<=n;j+=4)
+        {
+            t2=_mm_loadu_ps(b[k]+j); //把内存中从b[k][j]开始的四个单精度浮点数加载到t2寄存器
+            t3=_mm_div_ps(t2,t1); //相除结果放到t3寄存器
+            _mm_storeu_ps(b[k]+j,t3); //把t3寄存器的值放回内存
+        }
+        for(j;j<n;j++) b[k][j]/=b[k][k];
+        b[k][k] = 1.0;
+        //complete the first part;
+
+        for(i = k+1;i<n;i++)
+        {
+            for(j = k+1;j<n;j++) b[i][j] -= b[i][k]*b[k][j];
+            b[i][k]=0;
+        }
+    }
+}
+
+void Gauss_Part2(int n)//对第二个部分进行SSE向量化并行算法
+{
+    int i,j,k;
+    __m128 t1,t2,t3,t4;
+    for(k=0;k<n;k++)
+    {
+        for(j = k;j<n;j++) c[k][j] /= c[k][k];
+
+        for(i = k+1;i<n;i++)
+        {
+            float temp2[4] = {c[i][k],c[i][k],c[i][k],c[i][k]};
+            t1 = _mm_loadu_ps(temp2);
+            for(j = k+1;j+4<n;j+=4)
+            {
+                t2 = _mm_loadu_ps(c[k]+j);
+                t3 = _mm_loadu_ps(c[i]+j);
+                t4 = _mm_mul_ps(t1,t2);
+                t3 = _mm_sub_ps(t3,t4);
+                _mm_storeu_ps(c[i]+j,t3);
+            }
+            for(j = j;j<n;j++) c[i][j] -= c[i][k]*c[k][j];
+            c[i][k]=0;
+        }
+    }
+}
+
+
+void Gauss_SSE(int n)
+{
+    int i,j,k;
+    __m128 t1,t2,t3,t4;
+    for(k=0;k<n;k++)
+    {
+        float temp[4] = {d[k][k],d[k][k],d[k][k],d[k][k]};
+        t1 = _mm_loadu_ps(temp);
+        for(j = k+1;j+4<=n;j+=4)
+        {
+            t2=_mm_loadu_ps(d[k]+j); //把内存中从d[k][j]开始的四个单精度浮点数加载到t2寄存器
+            t3=_mm_div_ps(t2,t1); //相除结果放到t3寄存器
+            _mm_storeu_ps(d[k]+j,t3); //把t3寄存器的值放回内存
+        }
+        for(j;j<n;j++) d[k][j]/=d[k][k];//处理不能被4整除的
+        d[k][k] = 1.0;
+
+        for(i = k+1;i<n;i++)
+        {
+            float temp2[4] = {c[i][k],c[i][k],c[i][k],c[i][k]};
+            t1 = _mm_loadu_ps(temp2);
+            for(j = k+1;j+4<n;j+=4)
+            {
+                t2 = _mm_loadu_ps(c[k]+j);
+                t3 = _mm_loadu_ps(c[i]+j);
+                t4 = _mm_mul_ps(t1,t2);
+                t3 = _mm_sub_ps(t3,t4);
+                _mm_storeu_ps(c[i]+j,t3);
+            }
+            for(j = j;j<n;j++) c[i][j] -= c[i][k]*c[k][j];
+            c[i][k]=0;
+        }
+
+    }
+}
+
+
+void Print(int n,float m[][2000])//打印结果
 {
     for(int i=0;i<n;i++)
     {
@@ -90,15 +180,12 @@ int main()
     {
         Initialize(n);
         count = 1;
-        if(n<=30)
-            cycle=1000;
+        if(n<=30) cycle=1000;
         else
-            if(n<=70)
-                cycle=100;
-            else
-                if(n>=200)
-                    cycle=10;
-                else cycle = 10;
+            if(n<=70) cycle=100;
+            else if(n<=300) cycle=50;
+            else cycle = 10;
+            
         QueryPerformanceFrequency(&tc1);
 		QueryPerformanceCounter(&t1);
 		while(count<cycle){
@@ -108,38 +195,17 @@ int main()
         QueryPerformanceCounter(&t2);
         cout<<n<<" "<<count<<" "<<((t2.QuadPart - t1.QuadPart)*1000.0 / tc1.QuadPart)<<endl;
         count=1;
+
+
         QueryPerformanceFrequency(&tc2);
+        QueryPerformanceCounter(&t3);
+        while(count<cycle)
+        {
+            Gauss_SSE(n);
+            count++;
+        }
+        QueryPerformanceCounter(&t4);
+        cout<<n<<" "<<count<<" "<<((t4.QuadPart - t3.QuadPart)*1000.0 / tc2.QuadPart)<<endl;
     }
     return 0;
 }
-// }
-// for(n=2;n<=N;n*=2){
-// 		Initialize(n);
-// 		count=1;
-// 		if(n<=30)
-//             cycle=1000;
-//         else
-//             if(n<=70)
-//                 cycle=100;
-//             else
-//                 if(n>=300)
-//                     cycle=10;
-
-// 		QueryPerformanceFrequency(&tc1);
-// 		QueryPerformanceCounter(&t1);
-// 		while(count<cycle){
-// 			Gauss_normal(n);
-//             count++;}
-//         QueryPerformanceCounter(&t2);
-// 		cout<<n<<" "<<count<<" "<<((t2.QuadPart - t1.QuadPart)*1000.0 / tc1.QuadPart);
-
-//         count=1;
-// 		QueryPerformanceFrequency(&tc2);
-// 		// QueryPerformanceCounter(&t3);
-// 		// while(count<cycle){
-// 		// 	Gauss_para(n);
-//         //     count++;}
-//         // QueryPerformanceCounter(&t4);
-// 		// cout<<" "<<((t4.QuadPart - t3.QuadPart)*1000.0 / tc2.QuadPart)<<endl;
-// 	}
-// }
